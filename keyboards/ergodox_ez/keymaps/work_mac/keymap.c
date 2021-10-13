aeea63a77f6aa530371fcfc663d3416b43d19e39
Alex Birdsall <alex.birdsall@indiegogo.com>
2021-10-13 16:40:49 -0700

    home row mods

--- keyboards/ergodox_ez/keymaps/work_mac/keymap.c
+++ keyboards/ergodox_ez/keymaps/work_mac/keymap.c
@@ -52,14 +52,14 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
-    KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           LALT_T(KC_LEFT),                                RALT_T(KC_RIGHT),KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSPACE,
-    LT(2,KC_TAB),   KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(1),                                          TG(1),          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
-    LCTL_T(KC_ESCAPE),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           LT(3,KC_SCOLON),LGUI_T(KC_QUOTE),
-    KC_LSHIFT,      LCTL_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,           LCTL(LGUI(KC_LALT)),                                LCTL(LGUI(KC_LALT)),KC_N,           KC_M,           KC_COMMA,       KC_DOT,         RCTL_T(KC_SLASH),KC_RSHIFT,
-    LT(3,KC_GRAVE), WEBUSB_PAIR,    LALT(KC_LSHIFT),KC_LGUI,        LT(2,KC_NO),                                                                                                    LT(1,KC_NO),    KC_DOWN,        KC_LBRACKET,    KC_RBRACKET,    MO(1),
-                                                                                                    LALT_T(KC_LEFT),KC_RIGHT,       KC_DOWN,        LCTL_T(KC_UP),
+    KC_GRAVE,          KC_1,         KC_2,           KC_3,           KC_4,           KC_5,           LALT_T(KC_LEFT),                                RALT_T(KC_RIGHT),KC_6,         KC_7,           KC_8,           KC_9,          KC_0,              KC_BSPACE,
+    LT(2,KC_TAB),      KC_Q,         KC_W,           KC_E,           KC_R,           KC_T,           TG(1),                                          TG(1),           KC_Y,         KC_U,           KC_I,           KC_O,          KC_P,              KC_BSLASH,
+    LCTL_T(KC_ESCAPE), LCTL_T(KC_A), LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   KC_G,                                                                            KC_H,         RSFT_T(KC_J),   RGUI_T(KC_K),   RALT_T(KC_L),  RCTL_T(KC_SCOLON), LGUI_T(KC_QUOTE),
+    KC_LSHIFT,         LCTL_T(KC_Z), KC_X,           KC_C,           KC_V,           KC_B,           LCTL(LGUI(LALT(KC_SPACE))),               LCTL(LGUI(KC_LALT)),KC_N,         LT(1,KC_M),     KC_COMMA,       KC_DOT,        LT(3,KC_SLASH),    KC_RSHIFT,
+    LT(3,KC_GRAVE),    WEBUSB_PAIR,  LALT(KC_LSHIFT),KC_LGUI,        LT(2,KC_NO),                                                                                                   KC_NO,          KC_DOWN,        KC_LBRACKET,   KC_RBRACKET,       MO(1),
+                                                                                                   LALT_T(KC_LEFT), KC_RIGHT,       KC_DOWN,        LCTL_T(KC_UP),
                                                                                                                     KC_END,         KC_PGUP,
-                                                                                    LGUI_T(KC_SPACE),LCTL_T(KC_ESCAPE),LALT_T(KC_MINUS),RALT_T(KC_EQUAL),RCTL_T(KC_ESCAPE),RGUI_T(KC_ENTER)
+                                                                         LGUI_T(KC_SPACE), LCTL_T(KC_ENTER), LALT_T(KC_MINUS),      RALT_T(KC_EQUAL), RCTL_T(KC_ENTER), RGUI_T(KC_SPACE)
   ),
   [1] = LAYOUT_ergodox_pretty(
     KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
