d159c93ecd1841efc0c2a8d44962415f371e68e4
Alex Birdsall <alex.birdsall@indiegogo.com>
2021-12-14 13:01:16 -0800

    Restore some original keys, fix nav layer delete-word, tweak symbol layout

--- keyboards/ergodox_ez/keymaps/work_mac/keymap.c
+++ keyboards/ergodox_ez/keymaps/work_mac/keymap.c
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ARR(KC_TAB),       KC_Q,         KC_W,           KC_E,         KC_R,         KC_T,     TT(1),            /**/    TT(1),            KC_Y,      KC_U,         KC_I,         KC_O,         KC_P,              KC_BSLASH,
     CTL_T(KC_ESCAPE),  CTL_T(KC_A),  ALT_T(KC_S),    GUI_T(KC_D),  SFT_T(KC_F),  KC_G,                       /**/                      KC_H,      SFT_T(KC_J),  GUI_T(KC_K),  ALT_T(KC_L),  CTL_T(KC_SCOLON),  KC_QUOTE,
     KC_LSHIFT,         MEDIA(KC_Z),  KC_X,           SYM(KC_C),    ARR(KC_V),    KC_B,     SPACEHAMMER,      /**/    SPACEHAMMER,      KC_N,      ARR(KC_M),    SYM(KC_COMMA),KC_DOT,       MEDIA(KC_SLASH),   KC_RSHIFT,
-    MEDIA(KC_GRAVE),   KC_QUOTE,     WEBUSB_PAIR,    KC_LEFT,      KC_RIGHT,                                 /**/                                 KC_NO,        KC_DOWN,      KC_LBRACKET,  KC_RBRACKET,       MO(1),
+    MEDIA(KC_GRAVE),   KC_QUOTE,     WEBUSB_PAIR,    KC_LEFT,      KC_RIGHT,                                 /**/                                 KC_UP,        KC_DOWN,      KC_LBRACKET,  KC_RBRACKET,       MO(1),
                                                                                    ALT_T(KC_LEFT), KC_RIGHT, /**/ KC_DOWN, MO(1),
                                                                                                      KC_END, /**/ KC_PGUP,
                                                           GUI_T(KC_SPACE), CTL_T(KC_ENTER), ALT_T(KC_MINUS), /**/ RALT_T(KC_EQUAL), CTL_T(KC_ENTER), GUI_T(KC_SPACE)
@@ -93,9 +93,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // SYM :: symbol layer
   [1] = LAYOUT_ergodox_pretty(
     KC_ESCAPE,         KC_F1,        KC_F2,          KC_F3,        KC_F4,        KC_F5,    KC_TRNS,          /**/    KC_TRNS,        KC_F6,       KC_F7,        KC_F8,        KC_F9,        KC_F10,            KC_F11,
-    KC_TRNS,           KC_EXLM,      KC_AT,          KC_LCBR,      KC_RCBR,      KC_PIPE,  KC_TRNS,          /**/    KC_TRNS,        KC_UP,       KC_7,         KC_8,         KC_9,         KC_ASTR,           KC_F12,
-    KC_TRNS,           KC_HASH,      KC_DLR,         KC_LPRN,      KC_RPRN,      KC_MINUS,                   /**/                    KC_DOWN,     KC_4,         KC_5,         KC_6,         KC_PLUS,           KC_TRNS,
-    KC_TRNS,           KC_PERC,      KC_CIRC,        KC_LBRACKET,  KC_RBRACKET,  KC_EQUAL, KC_TRNS,          /**/    KC_TRNS,        KC_AMPR,     KC_1,         KC_2,         KC_3,         KC_BSLASH,         KC_TRNS,
+    KC_TRNS,           KC_EXLM,      KC_AT,          KC_LBRACKET,  KC_RBRACKET,  KC_PIPE,  KC_TRNS,          /**/    KC_TRNS,        KC_UP,       KC_7,         KC_8,         KC_9,         KC_ASTR,           KC_F12,
+    KC_TRNS,           KC_HASH,      KC_DLR,         KC_LCBR,      KC_RCBR,      KC_MINUS,                   /**/                    KC_DOWN,     KC_4,         KC_5,         KC_6,         KC_PLUS,           KC_TRNS,
+    KC_TRNS,           KC_PERC,      KC_CIRC,        KC_LPRN,      KC_RPRN,      KC_EQUAL, KC_TRNS,          /**/    KC_TRNS,        KC_AMPR,     KC_1,         KC_2,         KC_3,         KC_BSLASH,         KC_TRNS,
     KC_TRNS,           KC_TILD,      KC_TRNS,        KC_TRNS,      KC_TRNS,                                  /**/                                 KC_TRNS,      KC_DOT,       KC_0,         KC_EQUAL,          KC_TRNS,
                                                                                            KC_TRNS, KC_TRNS, /**/ KC_TRNS, KC_TRNS,
                                                                                                     KC_TRNS, /**/ KC_TRNS,
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   // ARR :: arrows and navigation
   [2] = LAYOUT_ergodox_pretty(
-    KC_TRNS,           ALT_T(KC_1),  ALT_T(KC_2),    ALT_T(KC_3),  ALT_T(KC_4),  ALT_T(KC_5),KC_TRNS,        /**/    KC_TRNS,        ALT_T(KC_6), ALT_T(KC_7), ALT_T(KC_8), ALT_T(KC_9),   KC_F3,          ALT_T(KC_BSPACE),
+    KC_TRNS,           ALT_T(KC_1),  ALT_T(KC_2),    ALT_T(KC_3),  ALT_T(KC_4),  ALT_T(KC_5),KC_TRNS,        /**/    KC_TRNS,        ALT_T(KC_6), ALT_T(KC_7), ALT_T(KC_8), ALT_T(KC_9),   KC_F3,          A(KC_BSPACE),
     KC_TRNS,           KC_TRNS,      KC_TRNS,        KC_TRNS,      KC_TRNS,      KC_TRNS,  KC_TRNS,          /**/    TO(3),          BK_WORD,     KC_PGDOWN,   KC_PGUP,     FWD_WORD,      KC_TRNS,        KC_TRNS,
     ALT_T(KC_BSPACE),  KC_TRNS,      KC_TRNS,        DLT_WORD,     FWD_WORD,     KC_TRNS,                    /**/                    KC_LEFT,     KC_DOWN,     KC_UP,       KC_RIGHT,      KC_TRNS,        KC_TRNS,
     KC_TRNS,           KC_TRNS,      KC_TRNS,        KC_TRNS,      KC_TRNS,      BK_WORD,  KC_TRNS,          /**/    KC_TRNS,        KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,       KC_TRNS,        KC_TRNS,
