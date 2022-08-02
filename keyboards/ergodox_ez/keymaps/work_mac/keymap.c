c9f8b7a60d39ebb07805b99b5e9611393a356067
Alex Birdsall <ambirdsall@gmail.com>
2022-08-02 11:28:08 -0700

    add space cadet shift/paren combos

--- keyboards/ergodox_ez/keymaps/work_mac/keymap.c
+++ keyboards/ergodox_ez/keymaps/work_mac/keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,          KC_1,         KC_2,           KC_3,         KC_4,         KC_5,     ALT_T(KC_LEFT),   /**/    RALT_T(KC_RIGHT), KC_6,      KC_7,         KC_8,         KC_9,         KC_0,              KC_BSPACE,
     ARR(KC_TAB),       KC_Q,         KC_W,           KC_E,         KC_R,         KC_T,     TT(1),            /**/    TT(1),            KC_Y,      KC_U,         KC_I,         KC_O,         KC_P,              KC_BSLASH,
     CTL_T(KC_ESCAPE),  CTL_T(KC_A),  ALT_T(KC_S),    GUI_T(KC_D),  SFT_T(KC_F),  KC_G,                       /**/                      KC_H,      SFT_T(KC_J),  GUI_T(KC_K),  ALT_T(KC_L),  CTL_T(KC_SCOLON),  KC_QUOTE,
-    KC_LSHIFT,         MEDIA(KC_Z),  KC_X,           SYM(KC_C),    ARR(KC_V),    KC_B,     SPACEHAMMER,      /**/    SPACEHAMMER,      KC_N,      ARR(KC_M),    SYM(KC_COMMA),KC_DOT,       MEDIA(KC_SLASH),   KC_RSHIFT,
+    KC_LSPO,           MEDIA(KC_Z),  KC_X,           SYM(KC_C),    ARR(KC_V),    KC_B,     SPACEHAMMER,      /**/    SPACEHAMMER,      KC_N,      ARR(KC_M),    SYM(KC_COMMA),KC_DOT,       MEDIA(KC_SLASH),   KC_RSPC,
     MEDIA(KC_GRAVE),   KC_QUOTE,     WEBUSB_PAIR,    KC_LEFT,      KC_RIGHT,                                 /**/                                 KC_UP,        KC_DOWN,      KC_LBRACKET,  KC_RBRACKET,       MO(1),
                                                                                    ALT_T(KC_LEFT), KC_RIGHT, /**/ KC_DOWN, MO(1),
                                                                                                      KC_END, /**/ KC_PGUP,
