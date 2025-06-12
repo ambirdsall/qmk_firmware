9059607af3b27f27a88fdafd97c4f739ef7c23b5
Alex Birdsall <ambirdsall@gmail.com>
2025-06-12 13:13:26 -0700

    Promote `PRO_L` to the right thumb's symbol layer

--- keyboards/zsa/ergodox_ez/keymaps/ergodox/keymap.c
+++ keyboards/zsa/ergodox_ez/keymaps/ergodox/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                               /**/
                                                                                    ALT_T(KC_LEFT), KC_RIGHT,  /**/ KC_DOWN, MO(SYM_L),
                                                                                                      KC_END,  /**/ KC_PGUP,
-                                                            PRO(KC_SPACE),   NAV(KC_ENTER), ALT_T(KC_MINUS),  /**/ RALT_T(KC_EQUAL),  NAV(KC_ENTER),   SYM(KC_SPACE)
+                                                            SYM(KC_SPACE),   NAV(KC_ENTER), ALT_T(KC_MINUS),  /**/ RALT_T(KC_EQUAL),  NAV(KC_ENTER),   PRO(KC_SPACE)
   ),
   [SYM_L] = LAYOUT_ergodox_pretty(
     KC_ESCAPE,         KC_F1,        KC_F2,          KC_F3,        KC_F4,        KC_F5,       KC_TRNS,        /**/    KC_TRNS,        KC_F6,       KC_F7,        KC_F8,        KC_F9,        KC_F10,            KC_F11,
