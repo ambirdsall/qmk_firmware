8daa72c612fe86e3833045867bc3b92195b1db70
Alex Birdsall <ambirdsall@gmail.com>
2025-07-21 13:25:39 -0700

    Extract SYST_L layer for firmware metashenanigans

--- keyboards/zsa/ergodox_ez/keymaps/ergodox/keymap.c
+++ keyboards/zsa/ergodox_ez/keymaps/ergodox/keymap.c
@@ -42,6 +42,7 @@ enum layers {
     VOL_L,  // the assumption behind naming this "Volume layer" is that those are the media control keys I'll use most
     NUM_L,  // "Num{ber,pad} layer"
     FUN_L,  // "Function [key] layer" is like a numpad, but for F1, F2, F3, et al
+    SYST_L, // "System layer" for firmware metashenanigans. Flashing, rebooting, maybe someday the rest of the iceberg.
 };
 
 #define SYM(kc) LT(SYM_L,kc)
@@ -51,6 +52,7 @@ enum layers {
 #define VOL(kc) LT(VOL_L,kc)
 #define NUM(kc) LT(NUM_L,kc)
 #define FUN(kc) LT(FUN_L,kc)
+#define SYST(kc) LT(SYST_L,kc)
 
 enum custom_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
@@ -65,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     NAV(KC_TAB),       KC_Q,         KC_W,           KC_E,         KC_R,         KC_T,        KC_HOME,        /**/   KC_PGUP,          KC_Y,      KC_U,         KC_I,         KC_O,         KC_P,              KC_BSLS,
     CTL_T(KC_ESCAPE),  CTL_T(KC_A),  ALT_T(KC_S),    GUI_T(KC_D),  SFT_T(KC_F),  KC_G,                        /**/                     KC_H,      SFT_T(KC_J),  GUI_T(KC_K),  ALT_T(KC_L),  CTL_T(KC_SCLN),    KC_QUOTE,
     SC_LSPO,           EMOU(KC_Z),   FUN(KC_X),      NUM(KC_C),    VOL(KC_V),    KC_B,        KC_END,         /**/   KC_PGDN,          KC_N,      PRO(KC_M),    SYM(KC_COMMA),KC_DOT,       EMOU(KC_SLASH),    SC_RSPC,
-    EMOU(KC_GRAVE),    KC_QUOTE,     KC_PSCR,        KC_LEFT,      KC_RIGHT,                                  /**/                                KC_UP,        KC_DOWN,      KC_LBRC,      KC_RBRC,           MO(SYM_L),
+    SYST(KC_GRAVE),    KC_QUOTE,     KC_PSCR,        KC_LEFT,      KC_RIGHT,                                  /**/                                KC_UP,        KC_DOWN,      KC_LBRC,      KC_RBRC,           MO(SYM_L),
                                                                                                               /**/
                                                                                    ALT_T(KC_LEFT), KC_RIGHT,  /**/ KC_DOWN, MO(SYM_L),
                                                                                                      KC_END,  /**/ KC_PGUP,
@@ -94,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                   KC_TRNS,  KC_TRNS, KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS
   ),
   [EMOU_L] = LAYOUT_ergodox_pretty(
-    KC_TRNS,           ALT_T(KC_1),  ALT_T(KC_2),    ALT_T(KC_3),  ALT_T(KC_4),  ALT_T(KC_5), KC_TRNS,        /**/    KC_TRNS,        ALT_T(KC_6),   ALT_T(KC_7),    ALT_T(KC_8), ALT_T(KC_9),       KC_TRNS,        QK_BOOT,
+    KC_TRNS,           ALT_T(KC_1),  ALT_T(KC_2),    ALT_T(KC_3),  ALT_T(KC_4),  ALT_T(KC_5), KC_TRNS,        /**/    KC_TRNS,        ALT_T(KC_6),   ALT_T(KC_7),    ALT_T(KC_8), ALT_T(KC_9),       KC_TRNS,        KC_TRNS,
     KC_TRNS,           KC_TRNS,      KC_TRNS,        KC_TRNS,      KC_TRNS,      KC_TRNS,   KC_TRNS,          /**/    KC_TRNS,        KC_MS_BTN1,    KC_MS_BTN2,     KC_TRNS,     KC_TRNS,           KC_TRNS,        KC_TRNS,
     KC_TRNS,           KC_TRNS,      KC_TRNS,        KC_TRNS,      KC_TRNS,      KC_TRNS,                     /**/                    KC_MS_LEFT,    KC_MS_DOWN,     KC_MS_UP,    KC_MS_RIGHT,       NEXT_TRACK,     PLAY_PAUSE,
     KC_TRNS,           KC_TRNS,      KC_TRNS,        KC_TRNS,      KC_TRNS,      KC_TRNS,   KC_TRNS,          /**/    KC_TRNS,        KC_MS_WH_LEFT, KC_MS_WH_DOWN,  KC_MS_WH_UP, KC_MS_WH_RIGHT,    KC_TRNS,        KC_TRNS,
@@ -149,6 +151,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                           KC_TRNS,   KC_TRNS, /**/ KC_TRNS, KC_TRNS,
                                                                                                      KC_TRNS, /**/ KC_TRNS,
                                                                                    KC_TRNS, KC_TRNS, KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+  [SYST_L] = LAYOUT_ergodox_pretty(
+    KC_NO,             KC_NO,        KC_NO,          KC_NO,        KC_NO,        KC_NO,     KC_NO,            /**/    KC_NO,          KC_NO,         KC_F10,         KC_F11,      KC_F12,            KC_NO,          QK_BOOT,
+    KC_NO,             KC_NO,        KC_NO,          KC_NO,        QK_RBT,       KC_NO,     KC_NO,            /**/    KC_NO,          KC_NO,         KC_F7,          KC_F8,       KC_F9,             KC_NO,          KC_NO,
+    KC_NO,             KC_NO,        KC_NO,          DB_TOGG,      KC_NO,        KC_NO,                       /**/                    KC_NO,         KC_F4,          KC_F5,       KC_F6,             KC_NO,          KC_NO,
+    KC_NO,             KC_NO,        KC_NO,          KC_NO,        KC_NO,        QK_BOOT,   KC_NO,            /**/    KC_NO,          KC_NO,         KC_F1,          KC_F2,       KC_F3,             KC_NO,          KC_NO,
+    KC_NO,             KC_NO,        KC_NO,          KC_NO,        KC_NO,                                     /**/                                   KC_NO,          KC_NO,       KC_NO,             KC_NO,          KC_NO,
+                                                                                                              /**/
+                                                                                          KC_NO,     KC_NO,   /**/ KC_NO,   KC_NO,
+                                                                                                     KC_NO,   /**/ KC_NO,
+                                                                                   KC_NO,   KC_NO,   KC_NO,   /**/ KC_NO,   KC_NO,   KC_TRNS
   ),
 };
 
