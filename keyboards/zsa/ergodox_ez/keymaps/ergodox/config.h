a9535c0c5b565d046860854ffedbad652d901596
Alex Birdsall <ambirdsall@gmail.com>
2025-11-05 17:20:53 -0800

    Add TAP_CODE_DELAY to mitigate BS raylib WONTFIX
    Maaannnnnnnn,,,,,,, fuck raylib for this one, seriously

--- keyboards/zsa/ergodox_ez/keymaps/ergodox/config.h
+++ keyboards/zsa/ergodox_ez/keymaps/ergodox/config.h
@@ -5,3 +5,7 @@
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define FIRMWARE_VERSION u8"bdAVj/6qgxq"
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+// I really shouldn't need to update my firmware just to work around a silly frigging
+// raylib input handling bug, but here we are
+#define TAP_CODE_DELAY 20
